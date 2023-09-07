e861786aacec5939542fab4d7b7ab21a2cdb1398
blue-pitaya <blue.pitaya@pm.me>
2023-09-07 16:20:34 +0200

    init

---
+++ src/main/java/dev/bluepitaya/repofetcher/RepofetcherApplication.java
@@ -0,0 +1,13 @@
+package dev.bluepitaya.repofetcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepofetcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepofetcherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/bluepitaya/repofetcher/RepofetcherApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.bluepitaya.repofetcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepofetcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
